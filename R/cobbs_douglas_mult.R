9dab653a5e366265631c9742c1baeccd4512fcb7
giacomovagni <giacomovagni@gmail.com>
2023-05-17 14:30:33 +0100

    new

--- R/cobbs_douglas_mult.R
+++ R/cobbs_douglas_mult.R
@@ -1,11 +1,30 @@
 
-# Giacomo Vagni
-# May 2023
-# Essex
-
-#
-library(tidyverse)
-#
+#' Solve for Y
+#' @name cobb_douglas_mult
+#' @param a a vector of alpha values
+#' @param px a vector of prices
+#' @param I the budget
+#' @returns A dataframe
+#' @examples
+#' # 4 goods
+#' I = 100
+#' a = c(0.1, 0.1, 0.4, 0.4)
+#' sum(a)
+#' px = c(1,1,2,2)
+#' cobb_douglas_mult(a = a, px = px, I = I)
+#'
+#' # if you want to visualize 2 goods you can remove the extra-goods from the Budget
+#' # let's focus on good x3 and x4
+#' I_new = 100 - ((20*2) + (20*2))
+#'
+#' # x3 and x4
+#' I = I_new
+#' a = c(0.4, 0.4) # keep the same alpha values
+#' px = c(2,2) # keep the same prices
+#'
+#' cobb_douglas_mult(a = a, px = px, I = I_new)
+#' # we can plot a bivariate plot for x3 and x4
+#' cobbs_douglas_utility(I = I_new, a = 0.4, b = 0.4, px = 2, py = 2)$optimal_bundle
 
 ############################################################
 ############################################################
@@ -20,17 +39,6 @@ cobb_douglas_mult = function(a, px, I){
   df = data.frame(goods = 1:length(px), I, a, px, x, U, Budget_respect = sum(px*x))
   return(df)
 }
-#
-
-# 4 goods #
-#I = 100
-#a = c(0.1, 0.1, 0.4, 0.4)
-#sum(a)
-#px = c(1,1,2,2)
-#
-
-#
-#cobb_douglas_mult(a = a, px = px, I = I)
 #
 
 ###########################################################################
--- R/cobbs_douglas_utility.R
+++ R/cobbs_douglas_utility.R
@@ -1,12 +1,16 @@
 
-#
-library(tidyverse)
-library(Deriv)
-library(ggthemes)
-#
+############################################################
+############################################################
 
-############################################################
-############################################################
+#' Solve for Y
+#' @name f_cobbs_y
+#' @param U the Utility
+#' @param x the quantity of x
+#' @param a the alpha value
+#' @param b the beta value
+#' @returns A dataframe
+#' @examples
+#' f_cobbs_y(x=0:10,U=5,a=0.5,b=0.5)
 
 #
 f_cobbs = function(x=x,y=y,a=a,b=b) (x^a) * (y^b)
@@ -20,13 +24,23 @@ f_budget = function(x,px,y,py) x*px + py*y
 f_budget_y = function(x,px,py, I) (I - x*px)/py
 #
 
-# price for x
-px = 3
-# price for y
-py = 2
-#
+############################################################
+############################################################
 
-# I = Budget
+
+#' Calculate the optimal bundle for two goods (x and y) given a budget constraint.
+#' \deqn{U(x,y) = x^a \cdot y^b}
+#' @name cobbs_douglas_utility
+#' @param I the budget constraint
+#' @param px the price of good x
+#' @param py the price of good y
+#' @param a the alpha value
+#' @param b the beta value
+#' @returns A list.
+#' @examples
+#' cobbs_douglas_utility(I=100, a=0.5, b=0.5, px = 1, py = 2)
+#' cobbs_douglas_utility(I=100, a=0.4, b=0.5, px = 1.2, py = 2)
+
 
 # general function to solve a Cobbs-Douglas Utility function #
 cobbs_douglas_utility = function(I, a=0.5, b=0.5, px, py){
